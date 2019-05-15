c6ee8b84d0c212dd416affd35e6fd2d91ec7a287
NTM-M <ntm.m9898@gmail.com>
2019-05-15 22:56:55 +0900

    [start]4-8

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'books/new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'books#top'
+
+  post 'books' => 'books#create'
+
+  get 'books' => 'books#index'
+
+  get 'books/:id' => 'books#show' , as: 'book'
+end
