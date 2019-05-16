e5abbe96f0fd2d59905543a1eebf86cf5bd903c6
NTM-M <ntm.m9898@gmail.com>
2019-05-16 14:21:58 +0900

    [finish]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,18 +5,43 @@ class BooksController < ApplicationController
 
   def create
   	book = Book.new(book_params)
-  	book.save
-  	redirect_to book_path(book.id)
+  	if book.save
+  		flash[:notice] = "Book was successfuly created."
+  		redirect_to book_path(book.id)
+  	else
+  		render("books/index")
+  	end
   end
 
   def index
   	@book = Book.new
-  	@books = Book.all
+  	@books = Book.all.order(created_at: :desc)
   end
 
   def show
   	@book = Book.find(params[:id])
   end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+  		flash[:notice] = "Book was successfuly created."
+  		redirect_to book_path(@book.id)
+  	else
+  		render("books_path")
+  	end
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_path
+  end
+
 
   private
 
